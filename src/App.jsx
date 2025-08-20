81296ddb9390e3efa5cf20fc06f26a14ffc98d99
Bhavya0122 <bhavyarora22@gmail.com>
2025-08-21 00:02:25 +0530

    code changes related basic setup and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Body from './components/Body'
 
 function App() {
-  return <div>
-  </div>
+  return <Body/>
   
 }
 
-export default App
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_large.jpg" 
+        alt="bg-img"/>
+    </div>
+  )
+}
+
+export default Login;
