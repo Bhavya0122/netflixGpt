0839dcbdda731cb165cfef2b5676691839fbb623
Bhavya0122 <bhavyarora22@gmail.com>
2025-09-10 22:41:59 +0530

    form validations added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import React from 'react';
 import Header from './Header';
+import { checkValidate } from '../utils/validate';
 
 const Login = () => {
   const[isSignInForm, setIsSignInForm] = useState(true);
+  const[errorMessage, setErrorMessage]=useState();
+
   const toggleSignInForm=()=>
   {
     setIsSignInForm(!isSignInForm);
   }
-  const handleButtonClick=()=>
+
+  const email = useRef(null);
+  const password = useRef(null);
+  const name=useRef(null);
+
+  const handleButtonClick=(e)=>
   {
+    e.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
+    console.log(name.current.value);
+    const message=checkValidate(email.current.value,password.current.value,name.current.value);
+    console.log(message);
+    setErrorMessage(message);
 
   }
   return (
@@ -21,20 +36,24 @@ const Login = () => {
     </div>
     <form className=' bg-black/80 absolute text-white w-3/12 my-40 mx-auto right-0 left-0 m-4'>
       <h1 className='font-bold text-3xl p-4'>{isSignInForm ? "Sign In" : "Sign Up" }</h1>
-      {!isSignInForm && <input type="text"
+
+      {!isSignInForm && <input type="text" ref={name}
        className='w-80 mx-5 my-3 p-4 border border-white bg-transparent'
        placeholder='Full Name'/> }
 
-      <input type="text"
+      <input type="text" ref={email}
        className='w-80 mx-5 my-3 p-4 border border-white bg-transparent'
        placeholder='Email or mobile Number'/>
 
-      <input type="password" 
+      <input type="password" ref={password}
       className='w-80 mx-5 my-3 p-4 border border-white' 
       placeholder='Password'/>
 
+      <p className='text-red-500 text-lg mx-5 my-2'>{errorMessage}</p>
+
       <button 
-      className='w-80 bg-red-600 mx-5 my-3 p-2' onClick={handleButtonClick}>
+      className='w-80 bg-red-600 mx-5 my-3 p-2'
+      onClick={handleButtonClick}>
       {isSignInForm ? "Sign In" : "Sign Up" }
       </button>
       <p className='p-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already registered? Sign In now" }</p>
